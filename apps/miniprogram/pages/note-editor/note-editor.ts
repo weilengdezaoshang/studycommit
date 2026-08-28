919f38a237658768442f8beb9520fa8f0eaafbd9
chenGuoFeng <chenguofeng@xk.design>
2026-08-28 13:17:37 +0800

    feat(miniprogram): 完善记录编辑页.

--- apps/miniprogram/pages/note-editor/note-editor.ts
+++ apps/miniprogram/pages/note-editor/note-editor.ts
@@ -2,10 +2,25 @@ import { MONITOR_EVENTS } from '../../constants/events'
 import { monitor } from '../../services/monitor-adapter'
 import { getMockPapersApi } from '../../services/mock-papers'
 
+const NOTE_DRAFT_STORAGE_KEY = 'studycommit.note-editor.draft'
+
 Page({
   data: {
+    statusBarHeight: 0,
     content: '',
+    editorDate: 'AUG 27',
+    isQuestionActive: false,
+    photoPath: '',
     isSaving: false,
+  },
+
+  onLoad() {
+    const systemInfo = wx.getSystemInfoSync()
+    const draft = wx.getStorageSync(NOTE_DRAFT_STORAGE_KEY)
+    this.setData({
+      statusBarHeight: systemInfo.statusBarHeight ?? 0,
+      content: typeof draft === 'string' ? draft : '',
+    })
   },
 
   onShow() {
@@ -13,7 +28,30 @@ Page({
   },
 
   onContentInput(event: WechatMiniprogram.Input) {
-    this.setData({ content: event.detail.value })
+    const content = event.detail.value
+    this.setData({ content })
+    wx.setStorageSync(NOTE_DRAFT_STORAGE_KEY, content)
+  },
+
+  cancelWriting() {
+    wx.navigateBack()
+  },
+
+  toggleQuestion() {
+    this.setData({ isQuestionActive: !this.data.isQuestionActive })
+  },
+
+  choosePhoto() {
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      success: (result) => {
+        this.setData({ photoPath: result.tempFilePaths[0] ?? '' })
+      },
+      fail: (error) => {
+        monitor.captureError(error, { action: MONITOR_EVENTS.NOTE_SAVE_FAILED })
+      },
+    })
   },
 
   async saveNote() {
@@ -33,6 +71,7 @@ Page({
 
     try {
       await getMockPapersApi().create({ content })
+      wx.removeStorageSync(NOTE_DRAFT_STORAGE_KEY)
       monitor.track(MONITOR_EVENTS.NOTE_SAVE_SUCCESS)
       wx.showToast({ title: '已记下', icon: 'success' })
       setTimeout(() => wx.navigateBack(), 300)
